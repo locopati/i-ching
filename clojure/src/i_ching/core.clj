49232d39f43a103d0079caaa795d1996e42fcb74
AndyKriger <andy.kriger@gmail.com>
2017-06-24 10:06:39 -0400

    Core code cleanup.

--- clojure/src/i_ching/core.clj
+++ clojure/src/i_ching/core.clj
@@ -1,30 +1,25 @@
 (ns i-ching.core)
-(require '[clojure.core.match :refer [match]])
 
-(declare hexagram-line yin-or-yang two-or-three yarrow-sticks split-sticks gather-sticks)
+(defn gather-sticks [sticks]
+  (let [sticks-mod (mod sticks 4)]
+    (if (zero? sticks-mod) 4 sticks-mod)))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println args "Hello, World!"))
+(defn split-sticks [sticks]
+  (let [left-min (+ 5 (int (rand 12))) ;; when we split, we hold onto a minimum number of sticks
+        right-min (+ 5 (int (rand 12)))
+        left (+ left-min (int (rand (- sticks left-min right-min))))
+        ;;left (int (rand sticks))
+        right (- sticks 1 left)]
+    (+ 1 (gather-sticks left) (gather-sticks right))))
 
-(defn hexagram []
-  (let [lines (repeatedly 6 hexagram-line)
-        changes (map yin-or-yang lines)
-        first-hex (vec (map first changes))
-        second-hex (vec (map second changes))]
-    (println lines changes)
-  (vector first-hex second-hex)))
-
-(defn yin-or-yang [value]
-  (case value
-    6 '(0 1)
-    7 '(1 1)
-    8 '(0 0)
-    9 '(1 0)))
-
-(defn hexagram-line []
-  (reduce + (map two-or-three (yarrow-sticks))))
+(defn yarrow-sticks
+  ([]
+   (yarrow-sticks '() 49))
+  ([accum sticks]
+   (if (= 3 (count accum))
+     accum
+     (let [split-count (split-sticks sticks)]
+       (yarrow-sticks (conj accum split-count) (- sticks split-count))))))
 
 (defn two-or-three [value]
   (case value
@@ -34,27 +29,17 @@
     9 2
     (throw (IllegalArgumentException. (str value " is not a valid value for split-sticks")))))
 
-(defn yarrow-sticks
-  ([]
-   (yarrow-sticks '() 49))
-  ([accum sticks]
-   (if (= 3 (count accum))
-     accum
-     (let [split-count (split-sticks sticks)]
-       (yarrow-sticks (conj accum split-count) (- sticks split-count)))
-     )))
+(defn hexagram-line []
+  (reduce + (map two-or-three (yarrow-sticks))))
 
-(defn split-sticks [sticks]
-  (let [left-min (+ 5 (int (rand 12)))
-        right-min (+ 5 (int (rand 12)))
-        left (+ left-min (int (rand (- sticks left-min right-min))))
-        ;;left (int (rand sticks))
-        right (- sticks 1 left)]
-    (+ 1 (gather-sticks left) (gather-sticks right))))
+(defn yin-or-yang [value]
+  (case value
+    6 '(0 1)
+    7 '(1 1)
+    8 '(0 0)
+    9 '(1 0)))
 
-(defn gather-sticks [sticks]
-  (let [sticks-mod (mod sticks 4)]
-    (if (zero? sticks-mod) 4 sticks-mod)))
-
-
-
+(defn hexagram []
+  (->> (repeatedly 6 hexagram-line)
+       (map yin-or-yang)
+       (apply map vector)))

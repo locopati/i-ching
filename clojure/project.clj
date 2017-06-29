7fc3e3b3a1a981891fded6b36c2e4b85b6505c32
AndyKriger <andy.kriger@gmail.com>
2017-06-29 17:21:46 -0400

    Added expectations library to the project

--- clojure/project.clj
+++ clojure/project.clj
@@ -2,11 +2,14 @@
   :description "I-Ching"
   :url "https://github.com/AndyKriger/i-ching/tree/master"
   :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-            [lein-ring "0.12.0"]]
+            [lein-ring "0.12.0"]
+            [lein-expectations "0.0.8"]
+            [lein-autoexpect "1.9.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.9"]
                  [cheshire "5.7.1"]
                  [compojure "1.6.0" :exclusions [ring/ring-core commons-fileupload]]]
-  :profiles {:dev {:dependencies [[ring "1.6.0"]]}}
+  :profiles {:dev {:dependencies [[ring "1.6.0"] ;; this is declared here otherwise eval breaks in emacs
+                                  [expectations "2.2.0-beta1"]]}}
   :ring {:handler i-ching.web/app}
   )

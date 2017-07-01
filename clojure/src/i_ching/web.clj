706d528a5bc33304bee98af8b6797dc731f0e63d
AndyKriger <andy.kriger@gmail.com>
2017-06-30 21:00:59 -0400

    Output I Ching reading as HTML using Hiccup. No CSS yet.

--- clojure/src/i_ching/web.clj
+++ clojure/src/i_ching/web.clj
@@ -1,8 +1,11 @@
 (ns i-ching.web
-  (:require [cheshire.core :as cheshire]
+  (:require [i-ching.core :as i-ching]
+            [cheshire.core :as cheshire]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [compojure.coercions :as coerce]))
+            [compojure.coercions :as coerce]
+            [ring.util.response :as response]
+            [hiccup.core :refer :all]))
 
 (def hexagrams
   "Store the 64 hexagrams info in a local cache as a sequence of maps. 
@@ -14,10 +17,61 @@
   [key val]
   (first (filter #(= (key %) val) @hexagrams)))
 
+(defn verse-commentary-view
+  "Display verse and commentary"
+  [verse-commentary]
+  [:div
+   [:div.verse (:verse verse-commentary)]
+   [:div.commentary (:commentary verse-commentary)]])
+
+(defn section-view
+  "Display a section that is composed of one or more verse & commentary"
+  ([section-name hexagram]
+   (section-view section-name hexagram nil))
+  ([section-name hexagram changing-lines]
+   [:div {:class section-name}
+    [:div.heading section-name]
+    (if (seq changing-lines)
+      (->> changing-lines ;; the lines section is a vector of verse-commentaries
+           (map #(nth (hexagram (keyword section-name)) %))
+           (map verse-commentary-view))
+      (verse-commentary-view (hexagram (keyword section-name))))]))
+
+(defn hexagram-view
+  "Display a single hexagram"
+  ([hexagram]
+   (hexagram-view hexagram nil))
+  
+  ([hexagram changing-lines]
+   (println (:king-wen-number hexagram) changing-lines)
+   [:div.hexagram-container
+    [:div.hexagram (:hexagram hexagram)]
+    [:div.chinese [:span.ideogram (:chinese hexagram)] [:span.pinyin (:pinyin hexagram)]]
+    [:div.name (get-in hexagram [:name :wilhelm])]
+    [:div.description
+     [:div.heading "description"]
+     [:div (:description hexagram)]]
+    (section-view "image" hexagram)
+    (section-view "judgment" hexagram)
+    (if (seq changing-lines) (section-view "lines" hexagram changing-lines))]))
+
+(defn consult-view
+  "Display an I Ching reading"
+  []
+  (let [hexagrams (i-ching/hexagram)
+        changing-lines (i-ching/changing-lines hexagrams)
+        [primary-hexagram related-hexagram]
+        (map #(hexagram-by :hexagram-binary %) hexagrams)]
+    (html
+     [:div#consult-container
+      (hexagram-view primary-hexagram changing-lines)
+      (if (seq changing-lines) (hexagram-view related-hexagram))])))
+
 (defroutes app
   "This is the main ring handler for the application."
   (GET "/hex/:num" [num :<< coerce/as-int]
        {:body (cheshire/generate-string (hexagram-by :king-wen-number num))})
-  (GET "/" []  "Hello World")
-  (GET "/favicon.ico" [] "")
- )
+  (GET "/consult" [] (consult-view))
+  (GET "/" [] (response/redirect "/consult"))
+  (route/resources "/")
+  (route/not-found "Page not found"))
